24204e3f8f02c87b2cd6ad78d9dd13a1bf741087
Melody Stern <melliemuse@gmail.com>
2019-11-03 12:48:47 -0600

    setup file structure

---
+++ src/scripts/main.js
@@ -0,0 +1,5 @@
+// Lightning Exercise 1: Create an object to represent your favorite dinner item (e.g. meatloaf, spaghetti, fried fish, gumbo). Each object should have a name property, but you can add any other properties that you like, such as size, weight, ethnicity, vegetarian boolean.
+
+
+
+// Lightning Exercise 2: Output all of the key/value pairs into the DOM inside an <article> element with a class of food.
